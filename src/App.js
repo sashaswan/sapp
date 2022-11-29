61dda998be426be0500a8317bed14d8d28f69047
Sasha Swan <alexandrio748@gmail.com>
2022-11-29 18:23:29 +0200

    unit_1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/style.css';
+import test from './images/test.png'
 
 function App() {
+
+  let text = 'test variable';
+  let text2 = '222'
+
+  const style = {
+    fontSize: '24px',
+    fontStyle: 'italic',
+    color: 'red'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1><code style={style}>app_1</code></h1>
+        <img src={test} alt='testImage' />
+        <ul>
+          <li>Hello</li>
+          <li>{text + text2}</li>
+        </ul>
+      </div>
+    </>
   );
 }
+
 
 export default App;
